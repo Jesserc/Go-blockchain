bf3549b723426aa0a45f41b102dfbffbfe6b2758
Jesserc <raymondjesse713@gmail.com>
2023-10-30 07:22:14 +0100

    update

--- foundation/blockchain/signature/signature.go
+++ foundation/blockchain/signature/signature.go
@@ -55,7 +55,7 @@ func Sign(value any, privateKey *ecdsa.PrivateKey) (v, r, s *big.Int, err error)
 // =============================================================================
 
 // stamp returns a hash of 32 bytes that represents this data with
-// the Ardan stamp embedded into the final hash.
+// the Jesserc stamp embedded into the final hash.
 func stamp(value any) ([]byte, error) {
 
 	// Marshal the data.
@@ -65,9 +65,8 @@ func stamp(value any) ([]byte, error) {
 	}
 
 	// This stamp is used so signatures we produce when signing data
-	// are always unique to the Ardan blockchain.
+	// are always unique to the Jesserc blockchain.
 	stamp := []byte(fmt.Sprintf("\x19Jesserc Signed Message:\n%d", len(data)))
-	// stamp := []byte(fmt.Sprintf("\x19Ardan Signed Message:\n%d", len(v)))
 
 	// Hash the stamp and txHash together in a final 32 byte array
 	// that represents the data.
